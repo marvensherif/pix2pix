28bef970049ec79c8ce938e44314b2d57fe5826f
marvensherif <48600234+marvensherif@users.noreply.github.com>
2025-05-13 21:06:08 +0300

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -21,3 +21,27 @@ def random_jitter(input_image, real_image):
     input_image = tf.image.flip_left_right(input_image)
     real_image = tf.image.flip_left_right(real_image)
     return input_image, real_image
+    
+def advanced_augmentations(input_image, real_image):
+    if tf.random.uniform(()) > 0.5:
+        input_image = tf.image.random_brightness(input_image, max_delta=0.2)
+        real_image = tf.image.random_brightness(real_image, max_delta=0.2)
+    
+    if tf.random.uniform(()) > 0.5:
+        input_image = tf.image.random_contrast(input_image, lower=0.8, upper=1.2)
+        real_image = tf.image.random_contrast(real_image, lower=0.8, upper=1.2)
+    
+    if tf.random.uniform(()) > 0.5:
+        input_image = tf.image.random_saturation(input_image, lower=0.8, upper=1.2)
+        real_image = tf.image.random_saturation(real_image, lower=0.8, upper=1.2)
+    
+    if tf.random.uniform(()) > 0.5:
+        input_image = tf.image.random_hue(input_image, max_delta=0.05)
+        real_image = tf.image.random_hue(real_image, max_delta=0.05)
+
+    if tf.random.uniform(()) > 0.5:
+        noise = tf.random.normal(shape=tf.shape(input_image), mean=0.0, stddev=0.05, dtype=tf.float32)
+        input_image = tf.clip_by_value(input_image + noise, 0.0, 1.0)
+
+    return input_image, real_image
+
